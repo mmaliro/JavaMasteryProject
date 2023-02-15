1be3200efc3568c325c64867c4b202ae0fb0046c
mmaliro <mmaliro@dev-10.com>
2023-02-15 15:00:11 -0500

    Mastery

--- DontWreckMyHouse/src/main/java/learn/domain/ReservationService.java
+++ DontWreckMyHouse/src/main/java/learn/domain/ReservationService.java
@@ -1,6 +1,54 @@
 package learn.domain;
 
+import learn.data.DataException;
+import learn.data.GuestRepository;
+import learn.data.HostRepository;
+import learn.data.ReservationRepository;
+import learn.models.Guest;
+import learn.models.Host;
+import learn.models.Reservation;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class ReservationService {
+
+    private final GuestRepository guestRepository;
+    private final HostRepository hostRepository;
+    private final ReservationRepository reservationRepository;
+
+    public ReservationService(GuestRepository guestRepository, HostRepository hostRepository, ReservationRepository reservationRepository) {
+        this.guestRepository = guestRepository;
+        this.hostRepository = hostRepository;
+        this.reservationRepository = reservationRepository;
+    }
+
+    public List<Reservation> findByHost(UUID host_id) throws DataException {
+        List<Reservation> result = reservationRepository.findByHost(host_id);
+        for (Reservation reservation : result) {
+            int guest_id = reservation.getGuest().getGuest_id();
+            reservation.setGuest(guestRepository.findById(guest_id));
+            reservation.setHost(hostRepository.findById(host_id));
+        }
+
+        return result;
+    }
+
+    public List<Reservation> findByGuest(Host host, Guest guest) throws DataException {
+        List<Reservation> result = reservationRepository.findByHost(host.getHost_id());
+        List<Reservation> guestReservations = new ArrayList<>();
+        for (Reservation reservation : result) {
+            int guest_id = reservation.getGuest().getGuest_id();
+            reservation.setGuest(guestRepository.findById(guest_id));
+            if (reservation.getGuest().getGuest_id() == guest.getGuest_id()) {
+                guestReservations.add(reservation);
+            }
+        }
+
+        return guestReservations;
+    }
 
 
 }
